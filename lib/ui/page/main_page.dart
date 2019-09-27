63967efe9e3811d8042f1e3cff6b70584e291caf
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2019-09-27 20:02:35 +0300

    implement scratch

--- lib/ui/page/main_page.dart
+++ lib/ui/page/main_page.dart
@@ -12,23 +12,30 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: CarouselSlider(
-        scrollDirection: Axis.vertical,
-        items: List.generate(10, (indexY) {
-          return CarouselSlider(
-            scrollDirection: Axis.horizontal,
-            items: List.generate(10, (indexX) {
-              return Container(
-                margin: EdgeInsets.all(8),
-                color: Colors.deepOrange,
-                child: Center(
-                  child: Text("$indexX,$indexY"),
-                ),
-              );
-            }),
-          );
-        }),
+      body: Center(
+        child: CarouselSlider(
+          aspectRatio: width / height,
+          scrollDirection: Axis.vertical,
+          items: List.generate(10, (indexY) {
+            return CarouselSlider(
+              aspectRatio: width / height,
+              scrollDirection: Axis.horizontal,
+              items: List.generate(10, (indexX) {
+                return Container(
+                  margin: EdgeInsets.all(8),
+                  color: Colors.deepOrange,
+                  child: Center(
+                    child: Text("$indexX,$indexY"),
+                  ),
+                );
+              }),
+            );
+          }),
+        ),
       ),
     );
   }
